642136ac27169f0a64c5216d4dae9b2b06ce5986
miatorresch <miatorresch@unal.edu.co>
2022-11-09 05:44:14 -0500

    FROND END se agregan los servicios crud para el manejo de usuarios

--- frontend/src/app/app.service.ts
+++ frontend/src/app/app.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+
+const API_URL =  "http://localhost:8080";
+const USUARIO_URL = API_URL + "/usuarios"; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(
+    private http:HttpClient
+  ) { 
+
+  }
+
+  getUsiarioAll(){
+    return this.http.get(USUARIO_URL);
+  }
+
+  create(usuario:any){
+    return this.http.post(USUARIO_URL, usuario);
+  }
+
+  update(id:string, usuario:any){
+    return this.http.put(USUARIO_URL + "/" + id, usuario);
+  }
+
+
+  delete(id:string){
+    return this.http.delete(USUARIO_URL + "/" + id);
+  }
+
 }
